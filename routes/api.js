6369bcee440cba7e725cf167be1bd51ddc513da6
Raffee <Raffee1989@gmail.com>
2021-04-23 13:58:12 -0700

    changes in server.js, api router, stat.js

--- routes/api.js
+++ routes/api.js
@@ -19,6 +19,38 @@ router.post("/api/workouts",function (req,res){
         res.json(err)
     })
 });
+router.get("/api/workouts/range",function(req,res){  
+    Workout.find()
+    .then(data =>{  
+        res.json(data)
+    })
+    .catch(err => { 
+        res.json(err)
+    })
+});
+
+router.post("/api/workouts/range",function (req,res){    
+    Workout.create({})
+    .then(data => res.json(data))
+    .catch(err => { 
+        res.json(err)
+    })
+});
+
+router.put("/api/workouts/:id",({body,params},res)=>{   
+    Workout.findByIdAndUpdate(  
+     params.id,
+     {$push:{exercises:body} },
+     {new: true,runValidators:true }
+    )
+    .then(data => res.json(data))
+    .catch(err => { 
+        res.json(err)
+    })
+});
+
+
+
 
 
 
